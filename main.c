abc4f8d261a8fcd1f51b0ebc813472e5026c234e
MichaelMyers0 <siracusa.augusta@gmail.com>
2025-05-03 12:19:53 +0300

    12 th commit

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include "ftp.h"
 #include "dbgc.c"
-int main()
+int main(int argc, char** argv)
 {
+	if (argc < 2)
+	{
+		fprintf(stderr, "ERROR: Provide ftp server name\n");
+		exit(1);
+	}
 	ftp ftp_server(".");
 	ftp_server.build();
 	dbgc c;
-	std::initializer_list<const char*> list{"."};
+	std::initializer_list<const char*> list{*(argv + 1)};
 	c.gather_files(list);
 	exit(0);
 }
